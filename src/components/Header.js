7113a15bb73048f9eb7ce1740e129993441b0994
KeyurChanchad <chanchadkeyur2001@gmail.com>
2024-05-18 18:59:52 +0530

    setup gh-pages

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-primary header-container">
+    <div className="container">
+    <div className='align-item-center'>
+    <a className="navbar-brand fw-bold text-white fs-4" href="#">
+      <svg className='mx-1' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hospital-fill" viewBox="0 0 16 16">
+      <path d="M6 0a1 1 0 0 0-1 1v1a1 1 0 0 0-1 1v4H1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h6v-2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5V16h6a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1h-3V3a1 1 0 0 0-1-1V1a1 1 0 0 0-1-1zm2.5 5.034v1.1l.953-.55.5.867L9 7l.953.55-.5.866-.953-.55v1.1h-1v-1.1l-.953.55-.5-.866L7 7l-.953-.55.5-.866.953.55v-1.1zM2.25 9h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 2 9.75v-.5A.25.25 0 0 1 2.25 9m0 2h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5a.25.25 0 0 1 .25-.25M2 13.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25zM13.25 9h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5a.25.25 0 0 1 .25-.25M13 11.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25zm.25 1.75h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5a.25.25 0 0 1 .25-.25"/>
+    </svg>
+        JetSetMed
+      </a>
+    </div>
+      
+     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul className="navbar-nav mb-2 mb-lg-0">
+          <li className="nav-item px-2 fs-5">
+            <a className="nav-link active text-white fw-normal" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item px-2 fs-5">
+            <a className="nav-link text-white fw-normal" href="/">About</a>
+          </li>
+          <li className="nav-item px-2 fs-5">
+            <a className="nav-link text-white fw-normal" href="/">MedicalKit</a>
+          </li>
+          <li className="nav-item px-2 fs-5">
+            <a className="nav-link text-white fw-normal" href="/">Pricing</a>
+          </li>
+        </ul>
+      </div>
+      <div className='float-right'>
+      <button type="button" className="btn btn-primary rounded-pill px-4 fw-bolder">Sign Up</button>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,206 @@
+import React from "react";
+import Header from "../components/Header";
+import Sample1 from "../assets/video/sample_1.mp4";
+import Sample2 from "../assets/video/sample_2.mp4";
+import "../styles/global.css";
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <div className="bg-container">
+        <Header />
+        <video id="background-video" className="videoTag" autoPlay loop muted>
+          <source src={Sample1} type="video/mp4" />
+        </video>
+        <div className="w-100 position-absolute text-box d-flex justify-content-center align-item-center flex-column m-0">
+          <div className="w-100 d-flex justify-content-center align-item-center">
+            <p className="b-font fw-medium text-center text-white w-75">
+              Lorem nostrud sint est dolore elit culpa.
+            </p>
+          </div>
+          <p className="text-center text-white">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+            culpa consectetur cupiditate voluptate aspernatur laboriosam.
+          </p>
+        </div>
+      </div>
+      <div style={{ background: "#e3eff3" }}>
+        <div className="container-fluid w-75 ">
+          <div className="title-desc">
+            <h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, et!
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              placeat excepturi ab, laudantium, sequi earum consequuntur
+              officiis maxime quae temporibus blanditiis itaque aspernatur
+              perspiciatis sit recusandae. Minima pariatur ipsam adipisci? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              praesentium quos accusamus, voluptate autem quae perferendis
+              eligendi ut enim corporis.
+            </p>
+          </div>
+          <div className="title-desc">
+            <h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, et!
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              placeat excepturi ab, laudantium, sequi earum consequuntur
+              officiis maxime quae temporibus blanditiis itaque aspernatur
+              perspiciatis sit recusandae. Minima pariatur ipsam adipisci? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              praesentium quos accusamus, voluptate autem quae perferendis
+              eligendi ut enim corporis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-container">
+        <video id="background-video" className="videoTag" autoPlay loop muted>
+          <source src={Sample2} type="video/mp4" />
+        </video>
+        <div className="w-100 position-absolute text-box d-flex justify-content-center align-item-center flex-column m-0 z-3">
+          <div className="w-100 d-flex justify-content-center align-item-center">
+            <p className="b-font fw-medium text-center text-white w-75">
+              JetSetMed - Personalized Affordable Medical Kits
+            </p>
+          </div>
+          <div className="align-self-center">
+            <button className="btn btn-primary rounded-pill px-5 py-2">
+              {" "}
+              Get Started{" "}
+            </button>
+          </div>
+        </div>
+        <img
+          src={require("../assets/image/sample_1.jpg")}
+          alt="medical kit"
+          className="card-img"
+          id="img-1"
+        />
+        <img
+          src={require("../assets/image/sample_1.jpg")}
+          alt="medical kit"
+          className="card-img"
+          id="img-2"
+        />
+        <img
+          src={require("../assets/image/sample_1.jpg")}
+          alt="medical kit"
+          className="card-img"
+          id="img-3"
+        />
+      </div>
+      <div style={{ background: "#e3eff3" }}>
+        <div className="container-fluid">
+          <h1 className="heading-1">How we do it better</h1>
+          <div className="card-container">
+            <div className="card">
+              <img src={require("../assets/image/sample_1.jpg")} alt="img" />
+              <p>Risk Assessment</p>
+            </div>
+            <div className="card">
+              <img src={require("../assets/image/sample_2.jpg")} alt="img" />
+              <p>Risk Assessment</p>
+            </div>
+            <div className="card">
+              <img src={require("../assets/image/sample_3.jpg")} alt="img" />
+              <p>Pre-trip consultation</p>
+            </div>
+            <div className="card">
+              <img src={require("../assets/image/sample_2.jpg")} alt="img" />
+              <p>Kit Delivery</p>
+            </div>
+            <div className="card">
+              <img src={require("../assets/image/sample_3.jpg")} alt="img" />
+              <p>Travelling</p>
+            </div>
+            <div className="card">
+              <img src={require("../assets/image/sample_2.jpg")} alt="img" />
+              <p>Telecommunication</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{ background: "#e3eff3" }}>
+        <div className="container-fluid">
+          <h1 className="heading-1">Make your trips hassel-free</h1>
+          <div className="subscription-container">
+            <div className="card">
+              <div className="title">
+                <h4>JetsetMed Free</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="price">
+                <span>
+                  <span class="material-symbols-outlined">currency_rupee</span>0
+                </span>
+              </div>
+              <button className="btn rounded-pill px-5 py-2">Get Started</button>
+              <div className="desc">
+                <span className="include">Fees</span>
+                <span>Includes</span>
+                <ul>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card">
+              <div className="title">
+                <h4>JetsetMed Free</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="price">
+                <span>
+                  <span class="material-symbols-outlined">currency_rupee</span>0
+                </span>
+              </div>
+              <button className="btn btn-primary rounded-pill px-5 py-2">Get Started</button>
+              <div className="desc">
+                <span className="include">Fees</span>
+                <span>Includes</span>
+                <ul>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card">
+              <div className="title">
+                <h4>JetsetMed Free</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="price">
+                <span class="material-symbols-outlined">currency_rupee</span>{" "}
+                <span className="ruppe">0</span>
+              </div>
+              <button className="btn btn-primary rounded-pill px-5 py-2">Get Started</button>
+              <div className="desc">
+                <span className="include">Fees</span>
+                <span>Includes</span>
+                <ul>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                  <li>Lorem ipsum dolor sit.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
